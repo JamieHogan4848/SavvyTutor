31c63f3d0e1b0236d2a7f73fb6a11be0f8679800
Jamie Work <jhogan4899@gmail.com>
2022-06-20 14:34:27 +0100

    project prep

--- app/src/main/java/savvytutor/MainActivity.kt
+++ app/src/main/java/savvytutor/MainActivity.kt
@@ -1,7 +1,14 @@
-package com.example.savvytutor
+package savvytutor
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.AttributeSet
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.annotation.Nullable
+import com.example.savvytutor.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
